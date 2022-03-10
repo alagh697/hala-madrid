bcf0d9057fb95819b5dc27968d6a4130bbb4efc5
alagh697 <37829292+alagh697@users.noreply.github.com>
2022-03-10 11:19:46 +0100

    1 commit by Alaaeddine

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,108 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import {  CurrencyEuroIcon,  HomeIcon, LogoutIcon, UserIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
 
 const Home: NextPage = () => {
+
+  const home = {
+    jerseyColor: 'bg-white',
+    textColor: 'text-black',
+    nameColor: 'text-blue-600',
+    borderIn: 'border-amber-300',
+    borderOut: 'border-blue-600'
+  } 
+  const away = {
+    jerseyColor: 'bg-blue-800',
+    textColor: 'text-white',
+    nameColor: 'text-white',
+    borderIn: 'border-orange-500',
+    borderOut: 'border-orange-500'
+  } 
+  const third = {
+    jerseyColor: 'bg-teal-600',
+    textColor: 'text-white',
+    nameColor: 'text-white',
+    borderIn: 'border-black',
+    borderOut: 'border-black'
+  } 
+  const human = {
+    jerseyColor: 'bg-black',
+    textColor: 'text-white',
+    nameColor: 'text-white',
+    borderIn: 'border-white',
+    borderOut: 'border-white'
+  } 
+  const [kit, setKit] = useState(home);
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className={`flex h-screen flex-col items-center justify-center w-screen overflow-hidden font-body
+    ${kit.jerseyColor} ${kit.textColor}`}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Hala Madrid!</title>
+        <link rel="icon" href="/MadridReal.png" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <header className="flex items-center p-4 space-x-3">
+      <nav>
+        <div className="flex items-center space-x-4">
+          <button 
+          onClick={() => setKit(home)}
+          className='kit-button'>
+            <HomeIcon className="w-8 h-8"/>
+            <p className="hidden lg:inline">Home</p>
+          </button>
+          <button 
+          onClick={() => setKit(away)}
+          className='kit-button'>
+            <LogoutIcon className="w-8 h-8"/>
+            <p className="hidden lg:inline">Away</p>
+          </button>
         </div>
+        </nav>
+        
+        <div className={`flex justify-center border-b-8 ${kit.borderOut} w-80 rounded-b-full`}>
+        <div className={`flex justify-center border-4 ${kit.borderIn} w-80 rounded-b-full rounded-t-3xl p-6`}>
+          <Link href="/"><a className="flex flex-col justify-center items-center">
+            <img className="w-32" src="/MadridReal.png"/>
+            <span className="text-2xl">¡Hala Madrid!</span>
+          </a></Link>
+        </div>
+        </div>
+
+        <nav >
+        <div className="flex items-center space-x-3">
+          <button 
+          onClick={() => setKit(third)}
+          className='kit-button'>
+            <CurrencyEuroIcon className="w-8 h-8"/>
+            <p className="hidden lg:inline">Third</p>
+            </button>
+          <button 
+          onClick={() => setKit(human)}
+          className='kit-button'>
+            <UserIcon className="w-8 h-8"/>
+            <p className="hidden lg:inline">Human</p>
+          </button>
+        </div>
+        </nav>
+
+      </header>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center text-center">
+      <img className="fixed top-1/3 right-2 w-40 md:w-52 lg:w-64" src="/MadridReal.png"/>
+      <div className="fixed top-2/3">
+      <div className={`flex flex-col items-center justify-center ${kit.nameColor}`}>
+        <span className="text-5xl font-bold uppercase">Benzema</span>
+        <span className="text-9xl font-bold">9</span>
+      </div>
+      </div>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <img className="w-20" src="/MadridUcl.png"/>
       </footer>
     </div>
   )
